5c8995e29d2ffb59679fcd79bc82d7e1fa9b8d5b
hashimoto5450 <t.hashimoto5450@gmail.com>
2025-03-17 12:58:28 +0900

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -2,7 +2,7 @@ import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
 import { setupAuth } from "./auth";
-import { scan as webScanner } from "@shared/scanners/webScanner";
+import { scan as webScanner } from "../shared/scanners/webScanner";
 import { insertScanSchema, insertCustomRuleSchema } from "@shared/schema";
 
 export async function registerRoutes(app: Express): Promise<Server> {
